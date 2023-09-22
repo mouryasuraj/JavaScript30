0870a9a48179ace10102a7cf32cc1647da2082f4
Suraj <mouryasuraj6266444@gmail.com>
2023-09-22 23:19:28 +0530

    started project: Menu

---
+++ 11. Menu/script.js
@@ -0,0 +1,13 @@
+// Images link
+[
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-1.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-2.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-3.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-4.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-5.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-6.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-7.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-8.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-9.jpeg',
+    'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-10.jpeg'
+]
